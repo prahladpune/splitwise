6ef202e75470503a570cfbc5df931156cb9418df
prahlad <prahlad.ceg@gmail.com>
2022-02-05 01:04:01 +0530

    added entities

--- src/main/java/com/prahlad/study/splitwise/models/BaseModel.java
+++ src/main/java/com/prahlad/study/splitwise/models/BaseModel.java
@@ -1,4 +1,31 @@
 package com.prahlad.study.splitwise.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@MappedSuperclass
 public class BaseModel {
+
+    @Id
+    private int id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDate modifiedAt;
+
 }
--- src/main/java/com/prahlad/study/splitwise/models/Expense.java
+++ src/main/java/com/prahlad/study/splitwise/models/Expense.java
@@ -4,7 +4,10 @@ package com.prahlad.study.splitwise.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+
 @Setter
 @Getter
-public class Expense {
+@Entity
+public class Expense extends BaseModel{
 }
